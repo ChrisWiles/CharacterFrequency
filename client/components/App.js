2e6c6e6867f11dc0559a1404228b9000b9c9d8da
Christopher Wiles <chriswiles87@gmail.com>
2016-09-25 17:01:24 -0500

    get colors in app

--- client/components/App.js
+++ client/components/App.js
@@ -1,28 +1,28 @@
 import React, {Component} from 'react'
 import TextInput from './TextInput'
-import BubbleChart from './BubbleChart'
+import BarChart from './BarChart'
 import {frequency} from '../models/API'
+import {generateColorPalette} from '../models/helpers'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      frequency : {}
+      charFrequency : false,
+      colors: false
     }
   }
 
   componentDidMount() {
-
     // Sync with Mongo DB on load
-    frequency()
-      .then(obj => this._frequency(obj.data))
+    frequency().then(obj => this._frequency(obj.data))
+    // get random colors
+    this.setState({colors: generateColorPalette()})
   }
 
   _frequency(chars) {
-    this.setState({frequency: chars})
-    console.log(this.state.frequency)
+    this.setState({charFrequency: chars})
   }
-
 
   render() {
     return (
@@ -31,6 +31,7 @@ export default class App extends Component {
           <div className="col-md-offset-2 col-md-8">
             <h1 className='text-center'>Character Frequency</h1>
             <TextInput setFrequency={this._frequency.bind(this)}/>
+            {this.state.charFrequency ? <BarChart {...this.state}/> : <h1>No Data</h1>}
           </div>
         </div>
       </div>
