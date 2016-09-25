50d126bc069a7415eb47361bc3f93e1882a78b55
Christopher Wiles <chriswiles87@gmail.com>
2016-09-25 09:38:14 -0500

    add if else

--- client/models/helpers.js
+++ client/models/helpers.js
@@ -1,6 +1,10 @@
 export const characterFrequency = (string) => {
-   return string.toLowerCase().split('').reduce((frequency, letter) => {
-      frequency[letter] = frequency[letter] + 1 || 1
-      return frequency
-   }, {})
+   if (string) {
+      return string.toLowerCase().split('').reduce((frequency, letter) => {
+         frequency[letter] = frequency[letter] + 1 || 1
+         return frequency
+      }, {})
+   } else {
+      return {}
+   }
 }
