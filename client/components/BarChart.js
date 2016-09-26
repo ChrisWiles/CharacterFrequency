4a585a869f1606d69472e1e9c006e49bc9a7c4b6
Christopher Wiles <chriswiles87@gmail.com>
2016-09-25 20:25:12 -0500

    sort by value and if values are equal sort by Character

--- client/components/BarChart.js
+++ client/components/BarChart.js
@@ -1,34 +1,92 @@
 import React, {Component} from 'react'
-import {VictoryBar} from 'victory'
+import {colorList} from '../models/helpers'
+import {VictoryBar, VictoryPie} from 'victory'
 
 export default class BarChart extends Component {
 
+  componentDidMount() {
+    // get svg from DOM and adjust viewBox
+    const shape = document.getElementsByTagName("svg")
+    shape[0].setAttribute("viewBox", "55 0 900 900")
+  }
+
   _data(chars) {
-     const mapped = []
-     let count = 0
+     let mapped = []
      const colors = this.props.colors
      for (let key in chars) {
         mapped.push({
-           x: count + 1,
            y: chars[key],
-           fill: colors[count],
-           label: `${key}`
+           label: `${key} (${chars[key]})`
         })
-        count++
      }
+
+     // sort by value and if values are equal sort by Character
+     mapped = mapped.sort((a, b) => {
+        if (a.y === b.y) {
+           return a.label.charCodeAt(0) - b.label.charCodeAt(0)
+        }
+        return a.y - b.y
+     })
+
+     // once sorted the x value can be attached which Victory only sorts by
+     mapped.forEach((e, i) => {
+        e['x'] = i
+        e['fill'] = colors[i]
+     })
      return mapped
   }
 
   render() {
     return (
       <VictoryBar
-        height={500}
-        padding={75}
-        style={{
-          labels: {fontSize: 20}
-        }}
+        colorScale={colorList}
+        scale={"linear"} // "linear", "time", "log", "sqrt"
+        horizontal={true}
+        height={900}
+        width={900}
+        padding={5}
+        style={{labels: {fontSize: 14, fill: 'white'}}}
         data={this._data(this.props.charFrequency)}
       />
     )
   }
 }
+// export default class BarChart extends Component {
+//
+//   _data(chars) {
+//      let mapped = []
+//      const colors = this.props.colors
+//      for (let key in chars) {
+//         mapped.push({
+//            y: chars[key],
+//            label: `${key} (${chars[key]})`
+//         })
+//      }
+//
+//      // sort by value and if values are equal sort by Character
+//      mapped = mapped.sort((a, b) => {
+//         if (a.y === b.y) {
+//            return a.label.charCodeAt(0) - b.label.charCodeAt(0)
+//         }
+//         return a.y - b.y
+//      })
+//
+//      // once sorted the x value can be attached which Victory only sorts by
+//      mapped.forEach((e, i) => {
+//         e['x'] = i
+//         e['fill'] = colors[i]
+//      })
+//      return mapped
+//   }
+//
+//   render() {
+//     return (
+//       <VictoryPie
+//         innerRadius={140}
+//         colorScale={colorList}
+//         style={{labels: {fontSize: 6, fill: 'white'}}}
+//         data={this._data(this.props.charFrequency)}
+//       />
+//     )
+//   }
+// }
